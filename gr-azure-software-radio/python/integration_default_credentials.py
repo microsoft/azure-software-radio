a836e1bde512a388a806174069d9b76e630ad5a5
markosly <markosly@gmail.com>
2021-11-05 16:07:47 -0700

    Add default_credentials block + integration tests for keyvault & default_credentials block. (#17)
    * Add integration tests for keyvault & default_credentials blocks
    
    * Fix issues brought up in PR
    
    Co-authored-by: Marko Slijepcevic <mslijepcevic@microsoft.com>

---
+++ gr-azure-software-radio/python/integration_default_credentials.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) Microsoft Corporation.
+# Licensed under the GNU General Public License v3.0 or later.
+# See License.txt in the project root for license information.
+#
+
+from gnuradio import gr_unittest
+import pytest
+import azure_software_radio.default_credentials
+
+
+class IntegrationDefaultCredentials(gr_unittest.TestCase):
+    """
+    Integration tests for default_credentials class
+    """
+
+    def test_environment_only(self):
+        """
+        Test authentication using environment vars only.
+        """
+        instance = azure_software_radio.default_credentials.get_default_creds(enable_cli_credential=False,
+                                                                              enable_environment=True,
+                                                                              enable_managed_identity=False,
+                                                                              enable_powershell=False,
+                                                                              enable_visual_studio_code=False,
+                                                                              enable_shared_token_cache=False,
+                                                                              enable_interactive_browser=False)
+        n_creds = len(instance.credentials)
+        self.assertEqual(n_creds, 1, f"Expected 1 credential, got: {n_creds} !")
+        self.assertTrue('EnvironmentCredential' in str(instance.credentials[0]))
+
+    def test_managed_identity_only(self):
+        """
+        Test authentication using managed identity only.
+        """
+        instance = azure_software_radio.default_credentials.get_default_creds(enable_cli_credential=False,
+                                                                              enable_environment=False,
+                                                                              enable_managed_identity=True,
+                                                                              enable_powershell=False,
+                                                                              enable_visual_studio_code=False,
+                                                                              enable_shared_token_cache=False,
+                                                                              enable_interactive_browser=False)
+        n_creds = len(instance.credentials)
+        self.assertEqual(n_creds, 1, f"Expected 1 credential, got: {n_creds} !")
+        self.assertTrue('ManagedIdentityCredential' in str(instance.credentials[0]))
+
+
+if __name__ == '__main__':
+    gr_unittest.run(IntegrationDefaultCredentials)
