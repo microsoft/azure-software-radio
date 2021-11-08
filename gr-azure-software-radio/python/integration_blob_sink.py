4258fb9e677a31f692ac1371a8a322611d46bdd4
pomeroy3 <crpomero@microsoft.com>
2021-11-08 09:46:22 -0500

    Blob unit and integration testing (#16)
    Splitting out unit and integration tests for blob blocks, associated with AB#7310

---
+++ gr-azure-software-radio/python/integration_blob_sink.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) Microsoft Corporation.
+# Licensed under the GNU General Public License v3.0 or later.
+# See License.txt in the project root for license information.
+#
+
+"""
+Integration tests for functions from blob_sink.py
+"""
+
+import os
+import uuid
+
+from azure.storage.blob import BlobServiceClient
+from gnuradio import gr, gr_unittest
+from gnuradio import blocks
+import numpy as np
+
+from azure_software_radio import blob_sink
+
+
+class IntegrationBlobSink(gr_unittest.TestCase):
+    """ Test case class for running integration tests on blob_sink.py
+    """
+
+    def setUp(self):
+        """ Pull a blob connection string from an environment variable.
+
+        Use this to set up a separate blob service client for testing.
+        """
+
+        self.blob_connection_string = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
+        self.blob_service_client = BlobServiceClient.from_connection_string(
+            self.blob_connection_string)
+
+        # Create a unique name for the container
+        self.test_blob_container_name = str(uuid.uuid4())
+        self.container_client = self.blob_service_client.create_container(
+            self.test_blob_container_name)
+
+        self.top_block = gr.top_block()
+
+    def tearDown(self):
+        self.top_block = None
+        # clean up after test
+        self.container_client.delete_container()
+        self.blob_service_client.close()
+
+    def test_round_trip_data_through_blob(self):
+        """ Upload known data to a blob using the blob_source block.
+
+        Read this data back using the azure blob API and confirm that the data wasn't corrupted.
+        """
+
+        blob_name = 'test-blob.npy'
+        block_len = 500000
+
+        src_data = np.arange(0, 2*block_len, 1, dtype=np.complex64)
+        src = blocks.vector_source_c(src_data)
+
+        op_block = blob_sink(authentication_method="connection_string",
+                             connection_str=self.blob_connection_string,
+                             container_name=self.test_blob_container_name,
+                             blob_name=blob_name,
+                             block_len=block_len,
+                             queue_size=4)
+
+        self.top_block.connect(src, op_block)
+        self.top_block.run()
+
+        # connect to the test blob container and download the file
+        # compare the file we downloaded against the samples in the vector source
+        blob_client = self.blob_service_client.get_blob_client(
+            container=self.test_blob_container_name,
+            blob=blob_name)
+
+        result_data = np.frombuffer(blob_client.download_blob().readall(), dtype=np.complex64)
+
+        self.assertEqual(src_data.tolist(), result_data.tolist())
+
+
+if __name__ == '__main__':
+    gr_unittest.run(IntegrationBlobSink)
--- gr-azure-software-radio/python/qa_blob_sink.py
+++ gr-azure-software-radio/python/qa_blob_sink.py
@@ -7,40 +7,30 @@
 #
 
 from gnuradio import gr, gr_unittest
-from gnuradio import blocks
-import numpy as np
-import os
 import uuid
-from azure.storage.blob import BlobServiceClient
 from azure_software_radio import blob_sink
 
 
 class qa_blob_sink(gr_unittest.TestCase):
 
     def setUp(self):
-        """ Pull a blob connection string from an environment variable.
-
-        Use this to set up a separate blob service client for testing.
-        """
-
-        self.blob_connection_string = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
-        self.blob_service_client = BlobServiceClient.from_connection_string(
-            self.blob_connection_string)
+        self.blob_connection_string = (
+            "DefaultEndpointsProtocol=https;AccountName=accountname;AccountKey=accountkey;"
+            + "EndpointSuffix=core.windows.net"
+        )
 
         # Create a unique name for the container
         self.test_blob_container_name = str(uuid.uuid4())
-        self.container_client = self.blob_service_client.create_container(
-            self.test_blob_container_name)
 
         self.tb = gr.top_block()
 
     def tearDown(self):
         self.tb = None
-        # clean up after test
-        self.container_client.delete_container()
-        self.blob_service_client.close()
 
     def test_instance(self):
+        '''
+        Ensure we don't throw errors in the constructor when given inputs with valid formats
+        '''
 
         instance = blob_sink(authentication_method="connection_string",
                              connection_str=self.blob_connection_string,
@@ -51,43 +41,7 @@ class qa_blob_sink(gr_unittest.TestCase):
 
         # really only checking that the init didn't throw an exception above, but adding the check
         # below to keep flake8 happy
-        self.assertTrue(instance is not None)
-
-    def test_round_trip_data_through_blob(self):
-        """ Upload known data to a blob using the blob_source block.
-
-        Read this data back using the azure blob API and confirm that the data wasn't corrupted.
-        """
-
-        blob_name = 'test-blob.npy'
-        block_len = 500000
-
-        # set up a vector source with known complex data
-        src_data = np.arange(0, 2*block_len, 1, dtype=np.complex64)
-        src = blocks.vector_source_c(src_data)
-
-        # set up a blob sink
-        op = blob_sink(authentication_method="connection_string",
-                       connection_str=self.blob_connection_string,
-                       container_name=self.test_blob_container_name,
-                       blob_name=blob_name,
-                       block_len=block_len,
-                       queue_size=4)
-
-        self.tb.connect(src, op)
-        # run the flowgraph
-        self.tb.run()
-
-        # connect to the test blob container and download the file
-        # compare the file we downloaded against the samples in the vector source
-        blob_client = self.blob_service_client.get_blob_client(
-            container=self.test_blob_container_name,
-            blob=blob_name)
-
-        result_data = np.frombuffer(blob_client.download_blob().readall(), dtype=np.complex64)
-
-        # check data
-        self.assertTrue((src_data == result_data).all())
+        self.assertIsNotNone(instance)
 
 
 if __name__ == '__main__':
