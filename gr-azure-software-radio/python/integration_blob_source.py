ab03865aaec3e85480f70c41b1b11a8489027423
pomeroy3 <crpomero@microsoft.com>
2021-11-23 13:44:55 -0500

    Updating blob source and sink blocks to support more datatypes and vector inputs (#23)
    Updating the blob source and sink blocks to support a wider variety of data types and also support vector inputs and outputs.

--- gr-azure-software-radio/python/integration_blob_source.py
+++ gr-azure-software-radio/python/integration_blob_source.py
@@ -34,15 +34,14 @@ class IntegrationBlobSource(gr_unittest.TestCase):
 
         Use this to set up a separate blob service client for testing.
         """
-        self.blob_connection_string = os.getenv(
-            'AZURE_STORAGE_CONNECTION_STRING')
+        self.blob_connection_string = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
         self.blob_service_client = BlobServiceClient.from_connection_string(
             self.blob_connection_string)
         # Create a unique name for the container
         self.test_blob_container_name = str(uuid.uuid4())
         self.container_client = self.blob_service_client.create_container(
             self.test_blob_container_name)
-        self.top_block = gr.top_block()
+        self.top_block = gr.top_block(catch_exceptions=False)
 
     # pylint: disable=invalid-name
     def tearDown(self):
@@ -53,37 +52,8 @@ class IntegrationBlobSource(gr_unittest.TestCase):
 
         # clean up after test
         self.container_client.delete_container()
+        self.container_client.close()
         self.blob_service_client.close()
-
-    def test_round_trip_data_through_blob(self):
-        """ Upload known data to a blob using the azure blob API.
-
-        Read this data back into a GNU Radio vector sink using the blob_source
-        block and confirm that the data wasn't corrupted.
-        """
-
-        blob_name = 'test-blob.npy'
-        num_samples = 1000000
-
-        # set up a vector source with known complex data
-        src_data = np.arange(0, num_samples, 1, dtype=np.complex64)
-        # connect to the test blob container and upload our test data
-        blob_client = self.blob_service_client.get_blob_client(
-            container=self.test_blob_container_name,
-            blob=blob_name)
-        blob_client.upload_blob(data=src_data.tobytes(), blob_type='BlockBlob')
-
-        op_block = BlobSource(authentication_method="connection_string",
-                              connection_str=self.blob_connection_string,
-                              container_name=self.test_blob_container_name,
-                              blob_name=blob_name,
-                              queue_size=4)
-        dst = blocks.vector_sink_c()
-
-        self.top_block.connect(op_block, dst)
-        self.top_block.run()
-
-        self.assertEqual(src_data.tolist(), dst.data())
 
     def test_read_from_nonexistent_blob(self):
         """
@@ -92,7 +62,9 @@ class IntegrationBlobSource(gr_unittest.TestCase):
 
         blob_name = 'test-blob.npy'
 
-        op_block = BlobSource(authentication_method="connection_string",
+        op_block = BlobSource(np_dtype=np.complex64,
+                              vlen=1,
+                              authentication_method="connection_string",
                               connection_str=self.blob_connection_string,
                               container_name=self.test_blob_container_name,
                               blob_name=blob_name,
@@ -106,6 +78,84 @@ class IntegrationBlobSource(gr_unittest.TestCase):
             # passed through the GNU Radio runtime and aren't directly accessible
             op_block.work(src_data, output_items)
 
+    def round_trip_test_helper(self, dtype, sink, vlen=1):
+        """ Helper script for running round trip tests against a variety of data types
+
+        Args:
+            dtype (np datatype object): what datatype to use for the source data array
+            item_size (int): what is the itemsize for the block, including vector length
+            sink (gnuradio vector sink block): vector sink type appropriate for the dtype
+        """
+        blob_name = 'test-blob.npy'
+        num_samples = 1000000
+
+        # set up a vector source with known complex data
+        src_data = np.arange(0, num_samples, 1, dtype=dtype)
+        # connect to the test blob container and upload our test data
+        blob_client = self.blob_service_client.get_blob_client(
+            container=self.test_blob_container_name,
+            blob=blob_name)
+        blob_client.upload_blob(data=src_data.tobytes(), blob_type='BlockBlob')
+
+        op_block = BlobSource(np_dtype=dtype,
+                              vlen=vlen,
+                              authentication_method="connection_string",
+                              connection_str=self.blob_connection_string,
+                              container_name=self.test_blob_container_name,
+                              blob_name=blob_name,
+                              queue_size=4,
+                              retry_total=0)
+
+        self.top_block.connect(op_block, sink)
+        self.top_block.run()
+
+        self.assertEqual(src_data.tolist(), sink.data())
+
+    def test_round_trip_complex_float_data_through_blob(self):
+        """
+        Upload known np.complex64 data to a blob using the azure blob API and
+        confirm it doesn't get corrupted
+        """
+
+        self.round_trip_test_helper(dtype=np.complex64,
+                                    sink=blocks.vector_sink_c())
+
+    def test_round_trip_float_data_through_blob(self):
+        """
+        Upload known np.float32 data to a blob using the azure blob API and
+        confirm it doesn't get corrupted
+        """
+
+        self.round_trip_test_helper(dtype=np.float32,
+                                    sink=blocks.vector_sink_f())
+
+    def test_round_trip_int32_data_through_blob(self):
+        """
+        Upload known np.int32 data to a blob using the azure blob API and
+        confirm it doesn't get corrupted
+        """
+
+        self.round_trip_test_helper(dtype=np.int32,
+                                    sink=blocks.vector_sink_i())
+
+    def test_round_trip_char_data_through_blob(self):
+        """
+        Upload known np.byte data to a blob using the azure blob API and
+        confirm it doesn't get corrupted
+        """
+
+        self.round_trip_test_helper(dtype=np.ubyte,
+                                    sink=blocks.vector_sink_b())
+
+    def test_round_trip_complex_float_vector_data_through_blob(self):
+        """
+        Upload known np.complex64 vector data to a blob using the azure blob API and
+        confirm it doesn't get corrupted
+        """
+        vlen = 10
+        self.round_trip_test_helper(dtype=np.complex64,
+                                    sink=blocks.vector_sink_c(vlen=vlen),
+                                    vlen=vlen)
 
 if __name__ == '__main__':
     gr_unittest.run(IntegrationBlobSource)
--- gr-azure-software-radio/python/qa_blob_sink.py
+++ gr-azure-software-radio/python/qa_blob_sink.py
@@ -7,8 +7,14 @@
 # See License.txt in the project root for license information.
 #
 
+from unittest.mock import DEFAULT, patch
 import uuid
+
 from gnuradio import gr, gr_unittest
+from gnuradio import blocks
+import numpy as np
+
+import azure_software_radio.blob_sink
 from azure_software_radio import BlobSink
 
 
@@ -33,7 +39,9 @@ class qa_BlobSink(gr_unittest.TestCase):
         Ensure we don't throw errors in the constructor when given inputs with valid formats
         '''
 
-        instance = BlobSink(authentication_method="connection_string",
+        instance = BlobSink(np_dtype=np.complex64,
+                            vlen=1,
+                            authentication_method="connection_string",
                             connection_str=self.blob_connection_string,
                             container_name=self.test_blob_container_name,
                             blob_name='test-instance',
@@ -43,6 +51,51 @@ class qa_BlobSink(gr_unittest.TestCase):
         # really only checking that the init didn't throw an exception above, but adding the check
         # below to keep flake8 happy
         self.assertIsNotNone(instance)
+
+    @patch.object(azure_software_radio.blob_sink, "blob_container_info_is_valid")
+    @patch.multiple(BlobSink,
+                    create_blob=DEFAULT,
+                    stage_block=DEFAULT,
+                    stop=DEFAULT)
+    def test_end_to_end_run(self, _, create_blob, stage_block, stop): # pylint: disable=unused-argument
+        '''
+        Test the block properly starts up, reads data from the blob data queue, and cleanly
+        shuts down
+        '''
+
+        stop.return_value = True
+
+        blob_name = 'test-blob.npy'
+        num_samples = 50000
+        block_len = 25000
+
+        src_data = np.arange(0, num_samples, 1, dtype=np.complex64)
+
+        src = blocks.vector_source_c(src_data)
+
+        op_block = BlobSink(np_dtype=np.complex64,
+                            vlen=1,
+                            authentication_method="connection_string",
+                            connection_str=self.blob_connection_string,
+                            container_name=self.test_blob_container_name,
+                            blob_name=blob_name,
+                            block_len=block_len,
+                            queue_size=4,
+                            retry_total=0)
+
+        self.tb.connect(src, op_block)
+
+        self.tb.run()
+
+        # confirm that our code called the "stage_block" function the expected number of times
+        # with the expected inputs
+        expected_uploads = [src_data[:block_len].view(np.byte).tolist(),
+                            src_data[block_len:].view(np.byte).tolist()]
+        # call_args_list is a list of call objects. Each call object acts like a tuple, with the first
+        # element of the tuple containing positional arguments. The positional arguments themselves are a tuple of all
+        # the arguments passed in, so we have a list of tuples of tuples.
+        result_uploads = [c[0][0].tolist() for c in stage_block.call_args_list]
+        self.assertListEqual(expected_uploads, result_uploads)
 
 
 if __name__ == '__main__':
