001d0685420bb1aea52db2d5c635ad962f7296bd
Marko Slijepcevic <mslijepcevic@microsoft.com>
2021-12-14 21:04:41 +0000

    Rest API block

---
+++ gr-azure-software-radio/python/qa_rest_api.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+
+from gnuradio import gr, gr_unittest
+# from gnuradio import blocks
+from rest_api import RestApi
+import requests
+import time
+from socket import socket
+
+
+class qa_rest_api(gr_unittest.TestCase):
+    '''
+    def __init__(self):
+        self.check_var1 = None
+        self.check_var2 = None
+        self.check_var3 = None
+        self.pi = None
+        self.val = None
+    '''
+    def get_free_port(self):
+        with socket() as s:
+            s.bind(('',0))
+            return s.getsockname()[1]
+
+    def test_instance(self):
+        port = self.get_free_port()
+        instance = RestApi(self, port)
+        self.assertIsNotNone(instance)
+
+    def test_get_vars(self):
+        self.check_var1 = 1
+        self.check_var2 = '2'
+        self.check_var3 = {'pi':3.14}
+        port = self.get_free_port()
+        instance = RestApi(self, port)
+        # give server a second to startup
+        time.sleep(1)
+        addr_str = 'http://127.0.0.1:' + str(port) + '/get-locals'
+        r = requests.get(addr_str)
+        for i in range(1,4):
+            check_str = f'check_var{i}'
+            self.assertEqual(check_str in r.text, True)
+        self.assertEqual('fake_var' in r.text, False)
+
+    def test_get_by_name(self):
+        self.pi = 3.14
+        port = self.get_free_port()
+        instance = RestApi(self, port)
+        # give server a second to startup
+        time.sleep(1)
+        p = "name=pi"
+        addr_str = 'http://127.0.0.1:' + str(port) + '/get-by-name'
+        r = requests.get(addr_str, params=p)
+        res = eval(r.text)
+        self.assertEqual(res['pi'],3.14)
+    
+    def test_set_by_name(self):
+        self.pi = 0
+        port = self.get_free_port()
+        instance = RestApi(self, port)
+        # give server a second to startup
+        time.sleep(1)
+        addr_str = 'http://127.0.0.1:' + str(port) + '/set-by-name'
+        r = requests.put(addr_str, json={"pi":3.14})
+        #print(r.text)
+        res = eval(r.text)
+        self.assertEqual(self.pi,3.14)
+        self.assertEqual(res['All'],'set')
+
+    def set_var(self, val):
+        self.val = val
+
+    def test_call_by_name(self):
+        self.val = None
+        port = self.get_free_port()
+        instance = RestApi(self, port)
+        # give server a second to startup
+        time.sleep(1)
+        addr_str = 'http://127.0.0.1:' + str(port) + '/call-by-name'
+        r = requests.put(addr_str, json={"set_var":99})
+        res = eval(r.text)
+        self.assertEqual(self.val,99)
+        self.assertEqual(res['Ran'],'ok')
+
+if __name__ == '__main__':
+    gr_unittest.run(qa_rest_api)
---
+++ gr-azure-software-radio/python/rest_api.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import threading
+import uvicorn
+from fastapi import FastAPI
+from gnuradio import gr
+
+
+class RestApi(gr.basic_block):
+
+    """ Initialize the rest_api block
+
+        Args:
+            tbself: This is a reference to the topblock self. This gives the rest_api block
+                    access to the variables & callbacks of the topblock.
+            port: The desired server listening port.
+
+    """
+    def __init__(self, tbself, port):
+        gr.basic_block.__init__(self,
+            name="rest_api",
+            in_sig=[],
+            out_sig=[])
+        app = FastAPI()
+        @app.get("/get-locals")
+        def get_locals():
+            return dir(tbself)
+
+        @app.get("/get-by-name")
+        def get_by_name(name):
+            value = getattr(tbself, name)
+            return {name : value}
+
+        @app.put("/set-by-name")
+        def set_by_name(set_dict: dict):
+            for name in set_dict:
+                if name not in dir(tbself):
+                    return {"Error": "Var not found"}
+                try:
+                    setattr(tbself, name, set_dict[name])
+                except Exception:
+                    return {"Error": f"could not set {name}"}
+            return {"All" : "set"}
+
+        @app.put("/call-by-name")
+        def call_by_name(set_dict: dict):
+            for name in set_dict:
+                if name not in dir(tbself):
+                    return {"Error": f"Func: {name} not found"}
+                try:
+                    func = getattr(tbself, name)
+                    func(set_dict[name])
+                except Exception:
+                    return {'Error':f'Could not set {name}'}
+            return {"Ran":"ok"}
+
+        self.server_thread = threading.Thread(target=uvicorn.run, args=(app,), kwargs={'port':port}, daemon=True)
+        self.server_thread.start()
