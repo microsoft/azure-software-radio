79b6e6ab962755a9b7e6259d40c5a5fb970e58f0
Marko Slijepcevic <mslijepcevic@microsoft.com>
2021-12-14 22:32:03 +0000

    pylint3

--- gr-azure-software-radio/python/qa_rest_api.py
+++ gr-azure-software-radio/python/qa_rest_api.py
@@ -1,4 +1,4 @@
-# pylint: disable=missing-function-docstring, no-self-use, missing-class-docstring, no-member
+# pylint: disable=missing-function-docstring, no-self-use, missing-class-docstring, no-member, eval-used, unused-variable
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
@@ -14,19 +14,12 @@ import time
 from socket import socket
 
 
-class qa_rest_api(gr_unittest.TestCase):
-    '''
-    def __init__(self):
-        self.check_var1 = None
-        self.check_var2 = None
-        self.check_var3 = None
-        self.pi = None
-        self.val = None
-    '''
+class QaRestApi(gr_unittest.TestCase):
+
     def get_free_port(self):
-        with socket() as s:
-            s.bind(('', 0))
-            return s.getsockname()[1]
+        with socket() as sock:
+            sock.bind(('', 0))
+            return sock.getsockname()[1]
 
     def test_instance(self):
         port = self.get_free_port()
@@ -42,35 +35,35 @@ class qa_rest_api(gr_unittest.TestCase):
         # give server a second to startup
         time.sleep(1)
         addr_str = 'http://127.0.0.1:' + str(port) + '/get-locals'
-        r = requests.get(addr_str)
+        resp = requests.get(addr_str)
         for i in range(1, 4):
             check_str = f'check_var{i}'
-            self.assertEqual(check_str in r.text, True)
-        self.assertEqual('fake_var' in r.text, False)
+            self.assertEqual(check_str in resp.text, True)
+        self.assertEqual('fake_var' in resp.text, False)
 
     def test_get_by_name(self):
-        self.pi = 3.14
+        self.pi_val = 3.14
         port = self.get_free_port()
         instance = RestApi(self, port)
         # give server a second to startup
         time.sleep(1)
-        para = "name=pi"
+        para = "name=pi_val"
         addr_str = 'http://127.0.0.1:' + str(port) + '/get-by-name'
-        r = requests.get(addr_str, params=para)
-        res = eval(r.text)
-        self.assertEqual(res['pi'], 3.14)
+        resp = requests.get(addr_str, params=para)
+        res = eval(resp.text)
+        self.assertEqual(res['pi_val'], 3.14)
 
     def test_set_by_name(self):
-        self.pi = 0
+        self.pi_val = 0
         port = self.get_free_port()
         instance = RestApi(self, port)
         # give server a second to startup
         time.sleep(1)
         addr_str = 'http://127.0.0.1:' + str(port) + '/set-by-name'
-        r = requests.put(addr_str, json={"pi": 3.14})
+        resp = requests.put(addr_str, json={"pi_val": 3.14})
         #print(r.text)
-        res = eval(r.text)
-        self.assertEqual(self.pi, 3.14)
+        res = eval(resp.text)
+        self.assertEqual(self.pi_val, 3.14)
         self.assertEqual(res['All'], 'set')
 
     def set_var(self, val):
@@ -83,10 +76,10 @@ class qa_rest_api(gr_unittest.TestCase):
         # give server a second to startup
         time.sleep(1)
         addr_str = 'http://127.0.0.1:' + str(port) + '/call-by-name'
-        r = requests.put(addr_str, json={"set_var": 99})
-        res = eval(r.text)
+        resp = requests.put(addr_str, json={"set_var": 99})
+        res = eval(resp.text)
         self.assertEqual(self.val, 99)
         self.assertEqual(res['Ran'], 'ok')
 
 if __name__ == '__main__':
-    gr_unittest.run(qa_rest_api)
+    gr_unittest.run(QaRestApi)
