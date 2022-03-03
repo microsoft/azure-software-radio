05da0882658a975a4171e485c216d5794610a9f6
jo-rivera <johrivera@microsoft.com>
2022-03-03 04:14:28 +0000

    extend timeout for intermittent failures

--- gr-azure-software-radio/python/integration_eventhub_source.py
+++ gr-azure-software-radio/python/integration_eventhub_source.py
@@ -93,7 +93,7 @@ class IntegrationEventhubSource(gr_unittest.TestCase):
             msg_debug_block.num_messages(), 0)
 
         self.tb.start()
-        time.sleep(1)
+        time.sleep(10)
         self.assertEqual(
             msg_debug_block.num_messages(), 1)
         source_block.stop()
@@ -155,7 +155,7 @@ class IntegrationEventhubSource(gr_unittest.TestCase):
         self.assertEqual(
             msg_debug_block.num_messages(), 0)
         self.tb.start()
-        time.sleep(5)
+        time.sleep(10)
         source_block.stop()
         self.tb.stop()
         self.tb.wait()
